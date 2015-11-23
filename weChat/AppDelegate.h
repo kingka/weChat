b61339439fa28c7ed8806e9e2a97c4f0b6e20b21
Imanol <jk199054@163.com>
2015-11-23 17:05:34 +0800

    initial project

---
+++ weChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weChat
+//
+//  Created by Imanol on 15/11/23.
+//  Copyright © 2015年 Imanol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
