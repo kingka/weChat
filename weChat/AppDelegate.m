090469729b0cdcb9793100d47b3f7c03b5afa9c1
Imanol <jk199054@163.com>
2015-11-24 15:41:22 +0800

    登陆

--- weChat/AppDelegate.m
+++ weChat/AppDelegate.m
@@ -7,39 +7,86 @@
 //
 
 #import "AppDelegate.h"
-
-@interface AppDelegate ()
-
+#import "XMPP.h"
+/*
+ 1 用户登录流程
+ 1.初始化XMPPStream
+ 2.连接服务器(传一个jid)
+ 3.连接成功，接着发送密码
+ // 默认登录成功是不在线的
+ 4.发送一个 "在线消息" 给服务器 ->可以通知其它用户你上线
+ */
+@interface AppDelegate ()<XMPPStreamDelegate>{
+    
+    XMPPStream *_xmppStream;
+}
+-(void)setupXmppStream;
+-(void)connectToHost;
+-(void)sendPwdToHost;
+-(void)sendOnlineMsg;
 @end
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [self setupXmppStream];
+    [self connectToHost];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+#pragma mark - public methods
+
+#pragma mark - private Methods
+#pragma mark 初始化XMPPStream
+-(void)setupXmppStream{
+    _xmppStream = [[XMPPStream alloc]init];
+    //1 设置jid resource 用户登录客户端设备登录的类型
+    XMPPJID *jid = [XMPPJID jidWithUser:@"zhangsan" domain:@"imanol.local" resource:@"iPhone"];
+    _xmppStream.myJID = jid;
+    //2 设置host
+    _xmppStream.hostName = @"127.0.0.1";
+    //3 设置端口 The default port is 5222.
+    _xmppStream.hostPort = 5222;
+    //4 设置代理
+    [_xmppStream addDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+-(void)connectToHost{
+    NSError *error = nil;
+    [_xmppStream connectWithTimeout:XMPPStreamTimeoutNone error:&error];
+    if(error){
+        NSLog(@"%@",error);
+    }else{
+        NSLog(@"发送jid 成功");
+    }
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+-(void)sendPwdToHost{
+    NSError *error = nil;
+    [_xmppStream authenticateWithPassword:@"123456" error:&error];
+    if(error){
+        NSLog(@"%@",error);
+    }
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+-(void)sendOnlineMsg{
+    //XMPP框架，已经把所有的指令封闭成对象
+    XMPPPresence *presence = [XMPPPresence presence];
+    [_xmppStream sendElement:presence];
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+#pragma mark - xmppstream delegate
+-(void)xmppStreamDidConnect:(XMPPStream *)sender{
+    NSLog(@"%s",__func__);
+    [self sendPwdToHost];
 }
 
+-(void)xmppStreamDidAuthenticate:(XMPPStream *)sender{
+    NSLog(@"%s",__func__);
+    [self sendOnlineMsg];
+}
+
+-(void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(DDXMLElement *)error{
+    NSLog(@"%s",__func__);
+}
 @end
